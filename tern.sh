b0584f6478844cbb305d6f9be1066a381efa6239
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2025-03-18 21:21:40 +0700

    Create tern.sh

---
+++ tern.sh
@@ -0,0 +1,58 @@
+read -p "Masukkan nama user untuk menjalankan executor (default: root): " EXECUTOR_USER
+EXECUTOR_USER=${EXECUTOR_USER:-root}
+
+read -sp "Masukkan PRIVATE_KEY_LOCAL: " PRIVATE_KEY_LOCAL
+echo ""
+
+INSTALL_DIR="/home/$EXECUTOR_USER/t3rn"
+SERVICE_FILE="/etc/systemd/system/t3rn-executor.service"
+ENV_FILE="/etc/t3rn-executor.env"
+
+mkdir -p "$INSTALL_DIR" && cd "$INSTALL_DIR"
+
+TAG=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+wget "https://github.com/t3rn/executor-release/releases/download/$TAG/executor-linux-$TAG.tar.gz"
+
+tar -xzf executor-linux-*.tar.gz
+cd executor/executor/bin
+
+sudo bash -c "cat > $ENV_FILE" <<EOL
+RPC_ENDPOINTS="{\"l2rn\": [\"https://b2n.rpc.caldera.xyz/http\"], \"arbt\": [\"https://arbitrum-sepolia.drpc.org\", \"https://sepolia-rollup.arbitrum.io/rpc\"], \"bast\": [\"https://base-sepolia-rpc.publicnode.com\", \"https://base-sepolia.drpc.org\"], \"opst\": [\"https://sepolia.optimism.io\", \"https://optimism-sepolia.drpc.org\"], \"unit\": [\"https://unichain-sepolia.drpc.org\", \"https://sepolia.unichain.org\"]}"
+EOL
+
+sudo chown -R "$EXECUTOR_USER":"$EXECUTOR_USER" "$INSTALL_DIR"
+sudo chmod 600 "$ENV_FILE"
+
+sudo bash -c "cat > $SERVICE_FILE" <<EOL
+[Unit]
+Description=t3rn Executor Service
+After=network.target
+
+[Service]
+User=$EXECUTOR_USER
+WorkingDirectory=$INSTALL_DIR/executor/executor/bin
+ExecStart=$INSTALL_DIR/executor/executor/bin/executor
+Restart=always
+RestartSec=10
+Environment=ENVIRONMENT=testnet
+Environment=LOG_LEVEL=debug
+Environment=LOG_PRETTY=false
+Environment=EXECUTOR_PROCESS_BIDS_ENABLED=true
+Environment=EXECUTOR_PROCESS_ORDERS_ENABLED=true
+Environment=EXECUTOR_PROCESS_CLAIMS_ENABLED=true
+Environment=EXECUTOR_MAX_L3_GAS_PRICE=100
+Environment=PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL
+Environment=ENABLED_NETWORKS=arbitrum-sepolia,base-sepolia,optimism-sepolia,l2rn
+EnvironmentFile=$ENV_FILE
+Environment=EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=true
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl daemon-reload
+sudo systemctl enable t3rn-executor.service
+sudo systemctl start t3rn-executor.service
+
+echo "✅ Executor berhasil diinstall dan dijalankan! Menampilkan log real-time..."
+sudo journalctl -u t3rn-executor.service -f --no-hostname -o cat
